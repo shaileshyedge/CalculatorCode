dfead1b34857d9006c682b1032e125f3c8526677
shaileshyedge <yedgeshailesh@gmail.com>
2016-09-19 15:22:56 -0700

    Rounding off bmi index

--- public/javascripts/CalcCntr.js
+++ public/javascripts/CalcCntr.js
@@ -19,51 +19,5 @@ app.controller('CalcControl', function($scope,$http)
         {
         })
 }
-    $scope.subtractnum = function ()
-    {
-        $http ({
-            method: "POST",
-            data: {
-                "first_num": $scope.first_num,
-                "second_num": $scope.second_num
-            },
-            url: "http://localhost:3000/CalcServerCntr/subtract"
-        }).then (function success(output)
-        { $scope.result = output.data;
-        },function err(err)
-        {
-        })
-    }
 
-    $scope.multiplynum = function ()
-    {
-        $http ({
-            method: "POST",
-            data: {
-                "first_num": $scope.first_num,
-                "second_num": $scope.second_num
-            },
-            url: "http://localhost:3000/CalcServerCntr/multiply"
-        }).then (function success(output)
-        { $scope.result = output.data;
-        },function err(err)
-        {
-        })
-    }
-
-    $scope.dividenum = function ()
-    {
-        $http ({
-            method: "POST",
-            data: {
-                "mass": $scope.mass,
-                "height": $scope.height
-            },
-            url: "http://localhost:3000/CalcServerCntr/divide"
-        }).then (function success(output)
-        { $scope.result = output.data;
-        },function err(err)
-        {
-        })
-    }
 });
